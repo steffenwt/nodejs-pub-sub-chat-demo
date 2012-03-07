ae81fa350557e22655a996eaf054c141a9126e97
Steffen W <steffen.witt80@googlemail.com>
2012-03-07 21:11:34 +0100

    'Join' is now a separate channel instead of a control command.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var app = require('http').createServer(handler)
-	, io = require('socket.io').listen(app)
-	, fs = require('fs')
+var app = require('http').createServer(handler),
+	io = require('socket.io').listen(app),
+	fs = require('fs');
 	
-	app.listen(80);
+	app.listen(80); //, "192.168.178.34");
 	
 	function handler(req, res) { // just return index.html
 		fs.readFile(__dirname + '/index.html',
@@ -16,21 +16,19 @@ var app = require('http').createServer(handler)
 				res.end(data);
 			});
 	}
-	
+
 	io.sockets.on('connection', function (socket) { // handler for incoming connections
 		socket.on('chat', function (data) {
 			var msg = JSON.parse(data);
-			switch (msg.action) {
-				case 'join': var reply = JSON.stringify({action: 'control', user: msg.user, msg: ' joined the channel' });
-							 socket.emit('chat', reply);
-							 socket.broadcast.emit('chat', reply);
-							 break;
-				case 'message': var reply = JSON.stringify({action: 'message', user: msg.user, msg: msg.msg });
-								socket.emit('chat', reply);
-								socket.broadcast.emit('chat', reply);
-								break;
-				
-			}
-			console.log(data);
+			var reply = JSON.stringify({action: 'message', user: msg.user, msg: msg.msg });
+			socket.emit('chat', reply);
+			socket.broadcast.emit('chat', reply);
+		});
+
+		socket.on('join', function(data) {
+			var msg = JSON.parse(data);
+			var reply = JSON.stringify({action: 'control', user: msg.user, msg: ' joined the channel' });
+			socket.emit('chat', reply);
+			socket.broadcast.emit('chat', reply);
 		});
 	});
--- app_redis.js
+++ app_redis.js
@@ -1,7 +1,7 @@
-var app = require('http').createServer(handler)
-, io = require('socket.io').listen(app)
-, fs = require('fs')
-, redis = require('redis')
+var app = require('http').createServer(handler),
+	io = require('socket.io').listen(app),
+	fs = require('fs'),
+	redis = require('redis');
 
 app.listen(80);
 
@@ -52,10 +52,10 @@ SessionController.prototype.rejoin = function(socket, message) {
 	});
 	var current = this;
 	this.sub.on('subscribe', function(channel, count) {
- 		var rejoin = JSON.stringify({action: 'control', user: current.user, msg: ' rejoined the channel' });
- 		current.publish(rejoin);
+		var rejoin = JSON.stringify({action: 'control', user: current.user, msg: ' rejoined the channel' });
+		current.publish(rejoin);
 		var reply = JSON.stringify({action: 'message', user: message.user, msg: message.msg });
-		current.publish(reply)
+		current.publish(reply);
 	});
 	this.sub.subscribe('chat');
 };
@@ -69,38 +69,41 @@ SessionController.prototype.publish = function(message) {
 };
 
 SessionController.prototype.destroyRedis = function() {
-	if (this.sub != null) this.sub.quit();
-	if (this.pub != null) this.pub.quit();
+	if (this.sub !== null) this.sub.quit();
+	if (this.pub !== null) this.pub.quit();
 };
 
 io.sockets.on('connection', function (socket) { // the actual socket callback
+	console.log(socket.id);
 	socket.on('chat', function (data) { // receiving chat messages
 		var msg = JSON.parse(data);
-		switch (msg.action) {
-			case 'join': 	var sessionController = new SessionController(msg.user);
-						 	socket.set('sessionController', sessionController);
-						 	sessionController.subscribe(socket);
-						 	break;
-			case 'message': socket.get('sessionController', function(err, sessionController) {
-								if (sessionController == null) {
-									// implicit login - socket can be timed out or disconnected
-									var newSessionController = new SessionController(msg.user);
-									socket.set('sessionController', newSessionController);									
-									newSessionController.rejoin(socket, msg);
-								} else {
-									var reply = JSON.stringify({action: 'message', user: msg.user, msg: msg.msg });
-									sessionController.publish(reply);
-								}
-							});
-							break;
-		}
+		socket.get('sessionController', function(err, sessionController) {
+			if (sessionController === null) {
+				// implicit login - socket can be timed out or disconnected
+				var newSessionController = new SessionController(msg.user);
+				socket.set('sessionController', newSessionController);
+				newSessionController.rejoin(socket, msg);
+			} else {
+				var reply = JSON.stringify({action: 'message', user: msg.user, msg: msg.msg });
+				sessionController.publish(reply);
+			}
+		});
+		// just some logging to trace the chat data
+		console.log(data);
+	});
+
+	socket.on('join', function(data) {
+		var msg = JSON.parse(data);
+		var sessionController = new SessionController(msg.user);
+		socket.set('sessionController', sessionController);
+		sessionController.subscribe(socket);
 		// just some logging to trace the chat data
 		console.log(data);
 	});
 
 	socket.on('disconnect', function() { // disconnect from a socket - might happen quite frequently depending on network quality
 		socket.get('sessionController', function(err, sessionController) {
-			if (sessionController == null) return;
+			if (sessionController === null) return;
 			sessionController.unsubscribe();
 			var leaveMessage = JSON.stringify({action: 'control', user: sessionController.user, msg: ' left the channel' });
 			sessionController.publish(leaveMessage);
